3b46b67c29265d4c58fa615497cc5cc4aea32338
monioni60-ops <monioni60@gmail.com>
2026-08-21 10:33:16 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -454,5 +454,7 @@ def init_db():
 
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    with app.app_context():
+        db.create_all()  # Автоматическое создание всех таблиц при старте
+    app.run(debug=True)
+
